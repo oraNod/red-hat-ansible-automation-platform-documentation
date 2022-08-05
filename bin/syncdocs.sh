c5d411fcbce93fd9002b722e1699e2309873ef47
Don Naro <dnaro@redhat.com>
2022-08-05 12:52:14 +0100

    jenkins-ify find

--- bin/syncdocs.sh
+++ bin/syncdocs.sh
@@ -7,5 +7,5 @@ rm -rf $target/downstream
 rm -rf $target/release-notes
 cp -r $source/downstream target/downstream
 cp -r $source/release-notes target/release-notes
-find $target/downstream/titles/ -mindepth 1 -maxdepth 2 -type d -exec ln -sr target/aap-common {} \\;
-find $target/downstream/titles/ -name master.adoc -exec sed -i -e 's|//include::aap-common|include::aap-common|g' {} \\;
+find target/downstream/titles/ -mindepth 1 -maxdepth 2 -type d -exec ln -sr target/aap-common {} \;
+find target/downstream/titles/ -name master.adoc -exec sed -i -e 's|//include::aap-common|include::aap-common|g' {} \;
